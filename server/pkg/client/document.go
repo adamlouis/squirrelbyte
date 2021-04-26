fd1b7b25d7b9be9ee0bcf972efce10edfb709887
Adam Suczewski <adam@suczewski.com>
2021-04-26 02:12:28 -0700

    doc cli

--- server/pkg/client/document.go
+++ server/pkg/client/document.go
@@ -1,1 +1,56 @@
 package client
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/adamlouis/squirrelbyte/server/pkg/model"
+)
+
+type DocumentClient interface {
+	Post(ctx context.Context, d *model.Document) (*model.Document, error)
+}
+
+func NewHTTPDocumentClient(url string) DocumentClient {
+	return &documentClient{
+		url: url,
+	}
+}
+
+type documentClient struct {
+	url string
+}
+
+func (dc *documentClient) Post(ctx context.Context, d *model.Document) (*model.Document, error) {
+	b, err := json.Marshal(d)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := http.Post(
+		fmt.Sprintf("%s/api/documents", dc.url),
+		"application/json",
+		bytes.NewBuffer(b),
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		b, _ := ioutil.ReadAll(res.Body)
+		return nil, fmt.Errorf("error posting document: %s: %s", res.Status, string(b))
+	}
+
+	doc := model.Document{}
+	err = json.NewDecoder(res.Body).Decode(&doc)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing document from document server: %v", err)
+	}
+
+	return &doc, nil
+}
--- server/pkg/model/job.go
+++ server/pkg/model/job.go
@@ -1,54 +1,57 @@
 package model
 
-// Document is the document resource
-type Document struct {
-	ID        string     `json:"id"`
-	Header    JSONObject `json:"header"`
-	Body      JSONObject `json:"body"`
-	CreatedAt string     `json:"created_at"`
-	UpdatedAt string     `json:"updated_at"`
+// Job is the job resource
+type Job struct {
+	ID     string     `json:"id"`
+	Name   string     `json:"name"`
+	Status string     `json:"status"`
+	Input  JSONObject `json:"input"`
+
+	ScheduledFor *string `json:"scheduled_for"`
+	SucceededAt  *string `json:"succeeded_at"`
+	ErroredAt    *string `json:"errored_at"`
+	ClaimedAt    *string `json:"claimed_at"`
+	CreatedAt    string  `json:"created_at"`
+	UpdatedAt    string  `json:"updated_at"`
 }
 
-// ListDocumentsQueryParams are the query params for listing documents
-type ListDocumentsQueryParams struct {
+// GetJobPathParams are the path params for getting jobs
+type GetJobPathParams struct {
+	JobID string
+}
+
+type ClaimJobRequest struct {
+	Names []string
+}
+
+type ClaimJobPathParams struct {
+	JobID string
+}
+
+type DeleteJobPathParams struct {
+	JobID string
+}
+
+type ReleaseJobPathParams struct {
+	JobID string
+}
+
+type SetJobSuccessPathParams struct {
+	JobID string
+}
+
+type SetJobErrorPathParams struct {
+	JobID string
+}
+
+// ListJobsQueryParams are the query params for listing documents
+type ListJobsQueryParams struct {
 	PageToken string `json:"page_token"`
 	PageSize  int    `json:"page_size"`
 }
 
-// ListDocumentsResponse is the response for listing documents
-type ListDocumentsResponse struct {
-	Documents     []*Document `json:"documents"`
-	NextPageToken string      `json:"next_page_token"`
-}
-
-// GetDocumentPathParams are the path params for getting documents
-type GetDocumentPathParams struct {
-	DocumentID string
-}
-
-// PutDocumentPathParams are the path parameters for putting documents
-type PutDocumentPathParams struct {
-	DocumentID string
-}
-
-// DeleteDocumentPathParams are that path params for deleting documents
-type DeleteDocumentPathParams struct {
-	DocumentID string
-}
-
-// QueryDocumentsRequest is the request body for querying documents
-type QueryDocumentsRequest struct {
-	Select    []interface{} `json:"select"`
-	GroupBy   []interface{} `json:"group_by"`
-	OrderBy   []interface{} `json:"order_by"`
-	Where     interface{}   `json:"where"`
-	Limit     int           `json:"limit"`
-	PageToken string        `json:"page_token"`
-}
-
-// QueryDocumentsResponse is the response body from querying documents
-type QueryDocumentsResponse struct {
-	Result        []interface{} `json:"result"`
-	NextPageToken string        `json:"next_page_token"`
-	Insights      JSONObject    `json:"insights"`
+// ListJobsResponse is the response for listing documents
+type ListJobsResponse struct {
+	Jobs          []*Job `json:"jobs"`
+	NextPageToken string `json:"next_page_token"`
 }
